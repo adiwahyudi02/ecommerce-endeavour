9780ec32f90b7ba1520983d2d57b7f3a6c00a8d0
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:39:20 +0700

    feat: create useInfiniteScroll composable for fetch more data

---
+++ composables/useInfiniteScroll.ts
@@ -0,0 +1,44 @@
+interface IUseInfiniteScroll {
+  fetchNextPage: () => Promise<any>;
+  isShouldFetch: boolean;
+}
+
+export function useInfiniteScroll({
+  fetchNextPage,
+  isShouldFetch,
+}: IUseInfiniteScroll) {
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 50 && isShouldFetch) {
+      fetchNextPage();
+    }
+  };
+
+  // Add event listener when component mounted
+  const enableInfiniteScroll = () => {
+    window.addEventListener("scroll", handleScroll, false);
+    window.dispatchEvent(new Event("scroll"));
+  };
+
+  // Remove event listener when component unmounted
+  const disableInfiniteScroll = () => {
+    window.removeEventListener("scroll", handleScroll);
+  };
+
+  onMounted(() => {
+    enableInfiniteScroll();
+  });
+
+  onUnmounted(() => {
+    disableInfiniteScroll();
+  });
+
+  return {
+    enableInfiniteScroll,
+    disableInfiniteScroll,
+  };
+}
