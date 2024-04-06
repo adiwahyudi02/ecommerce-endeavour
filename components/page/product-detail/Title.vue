9fe0afbca8ff1fe87e98972976a5d3369db8f72b
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:07:03 +0700

    feat: create Title component for Product Detail

---
+++ components/page/product-detail/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="title">
+    {{ title }}
+  </h1>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<style lang="sass" scoped>
+.title
+  font-size: $font-size-xl
+  font-weight: lighter
+  margin-top: $spacing-sm
+</style>
