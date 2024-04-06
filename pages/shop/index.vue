3c8b8d37da4363ebf3b0395c52fdae5732cd99f3
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:21:05 +0700

    feat: create your shop page

---
+++ pages/shop/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <ProductList :data="data" :is-loading="isLoading" />
+</template>
+
+<script setup lang="ts">
+const shopStore = useShopStore();
+const { fetchShopProducts } = shopStore;
+const { products: data, isLoading } = storeToRefs(shopStore);
+
+await fetchShopProducts();
+</script>
