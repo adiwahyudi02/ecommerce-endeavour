df7f71b89c061491b2f00c0cb9b326b6ab9bd172
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:10:42 +0700

    feat: create Shipping Warehouse component for Product Detail

---
+++ components/page/product-detail/ShippingWarehouse.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="shipment__wrapper">
+    <p class="shipment__label">Shipping Warehouse</p>
+    <Card class="shipment__card">
+      <p class="shipment__warehouse">
+        {{ title }}
+      </p>
+      <p class="shipment__address">
+        {{ address }}
+      </p>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  address: string;
+}>();
+</script>
+
+<style lang="sass" scoped>
+.shipment
+  &__label
+    color: $black-900
+    font-weight: bold
+
+  &__card
+    margin-inline: 0
+    padding: $spacing-sm
+    box-shadow: $box-shadow-xs
+    background: $black-50
+
+    +responsive($breakpoint-md)
+      width: 50%
+
+  &__warehouse
+    font-size: $font-size-xs
+    font-weight: bold
+    margin-block: $spacing-sm
+
+  &__address
+    font-size: $font-size-xs
+    margin-block: $spacing-sm
+</style>
