0f8103cb1f69d8df353fa2078270e0165adf716b
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-07 01:00:26 +0700

    feat: create banner component

---
+++ components/common/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <Loading v-if="isLoading" />
+    <NoData v-else-if="!isLoading && !data.length"
+      >There is no banner list</NoData
+    >
+    <div v-else>
+      <Swiper
+        :modules="[SwiperPagination, SwiperAutoplay]"
+        :slides-per-view="1"
+        :loop="true"
+        :autoplay="{
+          delay: 3000,
+          disableOnInteraction: true,
+        }"
+        :pagination="{ clickable: true }"
+        space-between="8px"
+      >
+        <SwiperSlide v-for="(item, index) in data" :key="index">
+          <NuxtImg :src="item.src" :alt="item.alt" class="swiper__image" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IImages {
+  src: string;
+  alt: string;
+}
+
+interface IBannerSliderProps {
+  data: IImages;
+  isLoading?: boolean;
+}
+
+withDefaults(defineProps<IBannerSliderProps>(), {
+  isLoading: false,
+});
+</script>
+
+<style lang="sass" scoped>
+.swiper
+  &__image
+    width: 100%
+    height: 32rem
+    object-fit: cover
+    border: 3px solid $black-300
+    border-radius: $border-radius-lg
+</style>
