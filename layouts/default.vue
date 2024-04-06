817c9dc06d54a563604d883e3af60dcfe792876f
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:27:18 +0700

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="default-layout">
+    <div class="default-layout__main">
+      <Navbar />
+      <div class="default-layout__content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="sass" scoped>
+.default-layout
+  +flexbox(row, center)
+
+  &__main
+    width: 100%
+    max-width: $breakpoint-md
+
+  &__content
+    margin-top: 5rem
+    margin-inline: $spacing-sm
+    +responsive($breakpoint-sm)
+      margin-inline: $spacing-md
+</style>
