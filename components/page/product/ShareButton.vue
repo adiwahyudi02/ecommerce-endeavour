1da901977f93c2239566319ca786de29e5accf91
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:13:03 +0700

    feat: create share button

---
+++ components/page/product/ShareButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <Button variant="outline" is-full @click="$emit('onShare')">{{
+    shareLabel
+  }}</Button>
+</template>
+
+<script setup lang="ts">
+interface IShareButtonProps {
+  shareLabel?: string;
+}
+
+withDefaults(defineProps<IShareButtonProps>(), {
+  shareLabel: "Share",
+});
+
+defineEmits<{
+  (e: "onShare"): void;
+}>();
+</script>
