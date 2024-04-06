16d90ab5542f9fd86e077d89499c0583ac481956
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:17:42 +0700

    feat: create blank layout

---
+++ layouts/blank.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="blank-layout">
+    <div class="blank-layout__main">
+      <div class="blank-layout__content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="sass" scoped>
+.blank-layout
+  +flexbox(row, center)
+
+  &__main
+    width: 100%
+    max-width: $breakpoint-md
+
+  &__content
+    margin-inline: $spacing-sm
+    +responsive($breakpoint-sm)
+      margin-inline: $spacing-md
+</style>
