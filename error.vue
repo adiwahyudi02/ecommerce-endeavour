a1e4d8e500244de64fa19662e8bb674d2c074dfe
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:30:26 +0700

    feat: create 404 page

---
+++ error.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout name="404">
+    <div>
+      <Button @click="$router.push('/')">Back to Catalog</Button>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ layouts/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notfound">
+    <div>
+      <h1>Oops! Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <div class="notfound__error__wrapper">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="sass" scoped>
+.notfound
+  width: 100%
+  height: 100vh
+  +flexbox(column, center, center, wrap, $spacing-xl)
+
+  &__error__wrapper
+    +flexbox(column, end, end, wrap, $spacing-xl)
+</style>
