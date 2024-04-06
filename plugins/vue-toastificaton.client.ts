6ce716fbcd8c3df058a47e27d0a2d221616e6545
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:48:39 +0700

    feat: add useToast composable and vue-toastification plugin

---
+++ plugins/vue-toastificaton.client.ts
@@ -0,0 +1,20 @@
+import Toast, { POSITION } from "vue-toastification";
+
+const options = {
+  position: POSITION.TOP_RIGHT,
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+};
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Toast, options);
+});
