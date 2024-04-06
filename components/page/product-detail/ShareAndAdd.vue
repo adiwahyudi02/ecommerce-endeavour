33dd9f765f679e394e6a690ef279109d9224c0fc
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:08:47 +0700

    feat: create Share and Add to shop Button component for Product Detail

---
+++ components/page/product-detail/ShareAndAdd.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="buttons__wrapper">
+    <ProductShareButton
+      :share-label="shareLabel"
+      @on-share="$emit('onShare')"
+    />
+    <ProductAddShopButton :add-label="addLabel" @on-add="$emit('onAdd')" />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IShareAndAddProps {
+  shareLabel?: string;
+  addLabel?: string;
+}
+
+withDefaults(defineProps<IShareAndAddProps>(), {
+  shareLabel: "Share",
+  addLabel: "Add to shop",
+});
+
+defineEmits<{
+  (e: "onShare"): void;
+  (e: "onAdd"): void;
+}>();
+</script>
+
+<style lang="sass" scoped>
+.buttons
+  &__wrapper
+    +flexbox(row, flex-start, center, stretch, $spacing-sm)
+</style>
