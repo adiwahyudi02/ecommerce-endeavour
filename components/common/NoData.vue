eb15b66d3ed493eabcd75ae11ab7abfe710ea2ec
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:19:32 +0700

    feat: create NoData component

---
+++ components/common/NoData.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nodata">
+    <slot> No Data List </slot>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.nodata
+  +flexbox(row, center,center)
+  margin: $spacing-lg
+  font-size: $font-size-lg
+  font-weight: bold
+  color: $black-300
+</style>
