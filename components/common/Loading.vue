f115213d6d522880f3b444686db0099dbe3b3f85
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:08:03 +0700

    feat: create Loading component

---
+++ components/common/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loading">
+    <slot> Loading... </slot>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.loading
+  +flexbox(row, center,center)
+  margin: $spacing-lg
+  font-size: $font-size-lg
+  font-weight: bold
+  color: $black-300
+</style>
