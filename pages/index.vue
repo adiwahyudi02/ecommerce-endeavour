48544ba6f739709f08b4f8826983865f9105f189
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:53:41 +0700

    feat: create catalog page - add product list - add highest commission product - add highest rating product

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ProductHighestCommission
+      :data="highestCommissionProducts"
+      :is-loading="isLoading && !highestCommissionProducts.length"
+    />
+    <ProductHighestRating
+      :data="highestRatingProducts"
+      :is-loading="isLoading && !highestRatingProducts.length"
+    />
+    <Divider />
+    <div class="product-list">
+      <ProductList :data="productList" :is-loading="isLoading" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = useProductsStore();
+const {
+  fetchNextPageProducts,
+  fetchHighestRatingProducts,
+  fetchHighestCommissionProducts,
+} = store;
+const {
+  products: productList,
+  highestRatingProducts,
+  highestCommissionProducts,
+  hasMore,
+  isLoading,
+} = storeToRefs(store);
+
+await fetchHighestRatingProducts();
+await fetchHighestCommissionProducts();
+
+useInfiniteScroll({
+  fetchNextPage: fetchNextPageProducts,
+  isShouldFetch: hasMore.value && !isLoading.value,
+});
+</script>
+
+<style lang="sass" scoped>
+.product-list
+  margin-top: $spacing-xl
+</style>
