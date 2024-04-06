3affd95a19a557daca00d626c646d98a09d838bd
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:10:13 +0700

    feat: create Description component for Product Detail

---
+++ components/page/product-detail/Description.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p class="description__label">Product Description</p>
+    <p class="description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  description: string;
+}>();
+</script>
+
+<style lang="sass" scoped>
+.description
+  font-size: $font-size-sm
+  white-space: pre-line
+  line-height: $line-height-md
+  color: $black-800
+  font-weight: lighter
+
+  &__label
+    color: $black-900
+    font-weight: bold
+</style>
