cbcb0b4ddd61d028e320d60d1d033c1aabcf7801
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:06:04 +0700

    feat: create Add To Shop Button component

---
+++ components/page/product/AddShopButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <Button is-full @click="$emit('onAdd')">{{ addLabel }}</Button>
+</template>
+
+<script setup lang="ts">
+interface IAddShopButtonProps {
+  addLabel?: string;
+}
+
+withDefaults(defineProps<IAddShopButtonProps>(), {
+  addLabel: "Add to shop",
+});
+
+defineEmits<{
+  (e: "onAdd"): void;
+}>();
+</script>
