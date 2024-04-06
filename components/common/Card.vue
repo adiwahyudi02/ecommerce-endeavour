446cd77059d814e7bed0eff196aba1849d5d3ff1
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:05:40 +0700

    feat: create Card component

---
+++ components/common/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.card
+  background: white
+  padding: $spacing-md
+  border-radius: $border-radius-md
+  border: 1px solid $black-200
+  box-shadow: $box-shadow-sm
+  margin: $spacing-md
+</style>
