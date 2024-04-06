a43e0b4449c4e9f6f5c04076066f7f951cebf3d8
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 23:05:30 +0700

    feat: create Brand Name component

---
+++ components/page/product/BrandName.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="brand-name">{{ name }}</p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<style lang="sass" scoped>
+.brand-name
+  font-size: $font-size-xs
+  font-weight: lighter
+  color: $primary
+  border-radius: $border-radius-lg
+  background: $teal-100
+  border: 1px solid $primary
+  width: fit-content
+  padding: $spacing-xs $spacing-md
+</style>
