a46f33893e5e78f0a904ffc16e4e29764c52f5ed
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:33:04 +0700

    feat: create Highest Commision Slider Product component

---
+++ components/page/product/HighestCommission.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Highest Commision</h2>
+    <ProductSlider :data="data" :columns="columns" :is-loading="isLoading" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IProductList } from "~/types/products";
+
+interface IHighesCommisionProps {
+  columns?: number;
+  data: IProductList;
+  isLoading?: boolean;
+}
+
+withDefaults(defineProps<IHighesCommisionProps>(), {
+  columns: 4,
+  isLoading: false,
+});
+</script>
+
+<style lang="sass" scoped></style>
