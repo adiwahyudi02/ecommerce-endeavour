1be1736d2d1e406a8c54e5bb89dd326f76805a59
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:33:25 +0700

    feat: create Highest Rating Slider Product component

---
+++ components/page/product/HighestRating.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Highest Rating</h2>
+    <ProductSlider :data="data" :columns="columns" :is-loading="isLoading" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IProductList } from "~/types/products";
+
+interface IHighesRatingnProps {
+  columns?: number;
+  data: IProductList;
+  isLoading?: boolean;
+}
+
+withDefaults(defineProps<IHighesRatingnProps>(), {
+  columns: 4,
+  isLoading: false,
+});
+</script>
+
+<style lang="sass" scoped></style>
