3a15483501d30d147691201dcc9788cc793ab7f2
Adi Wahyudi <adi.whyd02@gmail.com>
2024-04-06 22:22:57 +0700

    feat: create Divider component

---
+++ components/common/Divider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="divider" />
+</template>
+
+<script setup lang="ts">
+interface IDividerProps {
+  borderWidth?: string;
+}
+
+withDefaults(defineProps<IDividerProps>(), {
+  borderWidth: "3px",
+});
+</script>
+
+<style lang="sass" scoped>
+.divider
+  border-bottom: v-bind(borderWidth) solid $black-100
+  margin-block: $spacing-md
+</style>
